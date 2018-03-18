516c2fac9cae421ca147d0826c4e9a281d023016
Jakub Duda <jakub.duda@performgroup.com>
2018-03-18 11:50:06 +0100

    asd

---
+++ config/coindata/dud4.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+    'invested' => 1600,
+    'items' => [
+        [
+            'symbol' => 'MIOTA',
+            'count' => 115,
+            'free' => 44,
+            'bid_price' => 3.5,
+        ],
+        [
+            'symbol' => 'ENG',
+            'count' => 31.62386249,
+            'bid_price' => 20.35,
+        ],
+        [
+            'symbol' => 'DOGE',
+            'count' => 10000,
+            'bid_price' => 0.0563470687,
+        ],
+        [
+            'symbol' => 'WAVES',
+            'count' => 13.93841851,
+            'bid_price' => 51.726,
+        ],
+        [
+            'symbol' => 'XVG',
+            'free' => 723.785 - 563,
+            'bid_amount' => 311,
+            'count' => 563,
+        ],
+        [
+            'symbol' => 'TRX',
+            'bid_amount' => 311,
+            'count' => 2667,
+            'free' => 2667 - 2413 - 30,
+        ],
+        [
+            'symbol' => 'LSK',
+            'bid_price' => 65.01,
+            'count' => 6.13444085,
+        ],
+    ],
+];
