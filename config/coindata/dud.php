26322272aa34638383d8eb23df6f5c11910354cb
Jakub Duda <jakub.duda@performgroup.com>
2018-03-21 14:43:18 +0100

    ETH

--- config/coindata/dud.php
+++ config/coindata/dud.php
@@ -7,16 +7,16 @@ return [
             'symbol' => 'LSK',
             'bid_price' => 65.01,
             'count' => 6.13444085,
-        ],
+        ],/*
         [
             'symbol' => 'ETH',
             'bid_price' => 2481,
             'count' => 0.35727487,
-        ],
+        ],*/
         [
             'symbol' => 'USDT',
             'bid_price' => 3.37,
-            'count' => 321.59 //+184.06 ,
+            'count' => 321.59 + 189.79,
         ],
     ],
 ];
