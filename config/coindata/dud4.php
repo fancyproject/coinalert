3ea2939d75af1be6d931bfcf74d3d027b0d18a68
Jakub Duda <kuba@fancyproject.pl>
2018-07-18 07:23:41 +0200

    Create dud4.php

---
+++ config/coindata/dud4.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'invested' => 1600,
+    'items' => [
+        [
+            'symbol' => 'LSK',
+            'bid_price' => 65.01,
+            'count' => 6.13444085,
+        ],
+        [
+            'symbol' => 'USDT',
+            'bid_price' => 3.69,
+            'count' => 370.92,
+        ],
+        [
+            'symbol' => 'USDT',
+            'bid_price' => 3.37,
+            'count' => 189.79,
+        ],
+    ],
+];
